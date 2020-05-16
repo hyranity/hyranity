651f8600ca6f2a08312a5eee922f7c7fdc3d8f2f
Johann <mast3r1sgr3at@hotmail.com>
2020-05-16 22:39:40 +0800

    test

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
 
-    var database = null;
-    var starCountRef = null;
-    var text = "";
+    // var database = null;
+    // var starCountRef = null;
+    // var text = "";
 
-    try {
-        database = firebase.database();
-        starCountRef = database.ref('test');
-    } catch (err) {
-        alert(err.message);
-    }
+    // try {
+    //     database = firebase.database();
+    //     starCountRef = database.ref('test');
+    // } catch (err) {
+    //     alert(err.message);
+    // }
 
 
-    try {
-        starCountRef.once('value', function (snapshot) {
+    // try {
+    //     starCountRef.once('value', function (snapshot) {
 
-            text = snapshot.val();
-            $('#testTitle').text(text);
-        });
-    } catch (error) {
-        alert(error.message)
-    }
+    //         text = snapshot.val();
+    //         $('#testTitle').text(text);
+    //     });
+    // } catch (error) {
+    //     alert(error.message)
+    // }
 
 
 
