4d971252589db60164ad4fc5cbf307e5fa6c4f49
Johann <mast3r1sgr3at@hotmail.com>
2020-05-17 20:31:15 +0800

    some change

--- config.js
+++ config.js
@@ -1,28 +1,3 @@
-$(document).ready(function () {
-
-    // var database = null;
-    // var starCountRef = null;
-    // var text = "";
-
-    // try {
-    //     database = firebase.database();
-    //     starCountRef = database.ref('test');
-    // } catch (err) {
-    //     alert(err.message);
-    // }
-
-
-    // try {
-    //     starCountRef.once('value', function (snapshot) {
-
-    //         text = snapshot.val();
-    //         $('#testTitle').text(text);
-    //     });
-    // } catch (error) {
-    //     alert(error.message)
-    // }
-
-
-
-    $('#testTitle').text(text);
-});
+$(".topEmpty").click(function(){
+    window.location.href = "index.html";
+}); 
